b67b4b46d677c7ef11b8e0a4d77b0b659392b806
Alecks Gates <agates@mail.agates.io>
2021-01-23 18:37:30 -0600

    Fix test after removing email field

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -146,7 +146,7 @@ it('register function smoke test', async () => {
     getRouter,
   });
   // Assert
-  expect(registerSetting).toHaveBeenCalledTimes(12);
+  expect(registerSetting).toHaveBeenCalledTimes(expectedSettings.length);
   expectedSettings.forEach((setting, index) => {
     expect(setting).toStrictEqual(
       registerSetting.mock.calls[index][0]
